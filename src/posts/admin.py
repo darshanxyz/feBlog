577658f8923dbf72f14a7060bb417b141bc71180
darshanxyz <darshanxyz@gmail.com>
2017-04-30 14:41:27 +0530

    Customized admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ['title', 'updated', 'posted']
+	list_display_links = ['updated']
+	list_editable = ['title']
+	list_filter = ['updated', 'posted']
+	search_fields = ['title', 'content']
+	class Meta:
+		model = Post
+
+admin.site.register(Post, PostModelAdmin)
--- src/posts/models.py
+++ src/posts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(max_length = 30)
 	content = models.TextField()
-	timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)
+	posted = models.DateTimeField(auto_now = False, auto_now_add = True)
 	updated = models.DateTimeField(auto_now = True, auto_now_add = False)
 
 	def __unicode__(self):
