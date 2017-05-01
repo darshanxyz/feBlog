b459b0aea6f2afd365f47e4db76b0fb075f71e6f
darshanxyz <darshanxyz@gmail.com>
2017-05-01 12:36:04 +0530

    Basic UI design

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -4,4 +4,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
+		widgets = {
+          'content': forms.Textarea(attrs={'rows':20, 'cols':100}),
+        }
 		fields = ['title', 'content']
