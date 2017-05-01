ad9168e1df6c3b0bb7c9a0803b08eecad7e0585b
darshanxyz <darshanxyz@gmail.com>
2017-05-01 11:29:12 +0530

    Static files setup

--- src/feBlog/urls.py
+++ src/feBlog/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from posts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	# url(r'^$', include('landing_page.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls', namespace = 'posts')),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
